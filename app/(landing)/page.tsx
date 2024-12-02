8ed82fbe63904ef196a38dc5a7a79319e2fb6335
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-02 17:37:40 +0100

    landing page created

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,14 @@
+import CategoriesBanner from "@/components/landing/categories-banner"
+import Hero from "@/components/landing/hero"
+import ProductsGrid from "@/components/landing/products-grid"
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className="bg-slate-200 min-h-screen w-full">
+      <Hero />
+      <CategoriesBanner />
+      <ProductsGrid />
+    </div>
   )
 }
 

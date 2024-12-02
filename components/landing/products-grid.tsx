b619cc135ed8c0ebbe372797f21cd57594d6711e
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-02 17:54:01 +0100

    added skeleton of product item to products grid

--- components/landing/products-grid.tsx
+++ components/landing/products-grid.tsx
@@ -1,15 +1,19 @@
+import ProductItem from "./product-item";
+
 const ProductsGrid = () => {
   return (
     <div className="flex justify-center items-center px-16 py-12 w-full">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-screen-2xl w-full">
-        <div className="bg-blue-500 w-[285px] h-[446px]">Card 1</div>
-        <div className="bg-green-500 w-[285px] h-[446px]">Card 2</div>
-        <div className="bg-red-500 w-[285px] h-[446px]">Card 3</div>
-        <div className="bg-yellow-500 w-[285px] h-[446px] pr-0">Card 4</div>
-        <div className="bg-purple-500 w-[285px] h-[446px]">Card 5</div>
-        <div className="bg-pink-500 w-[285px] h-[446px]">Card 6</div>
-        <div className="bg-indigo-500 w-[285px] h-[446px]">Card 7</div>
-        <div className="bg-teal-500 w-[285px] h-[446px] pr-0">Card 8</div>
+
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
       </div>
     </div>
   );

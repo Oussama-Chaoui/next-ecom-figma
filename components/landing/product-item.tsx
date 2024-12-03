77b1bee2fd9c806f0cafaf9aab95f373c055b9f2
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-03 16:53:03 +0100

    finished product item and products grid

--- components/landing/product-item.tsx
+++ components/landing/product-item.tsx
@@ -1,14 +1,50 @@
+'use client'
 import { cn } from "@/lib/utils";
+import { ArrowLeftRight, Heart, Share } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Image from "next/image"
+import { MouseEventHandler, useState } from "react";
 
 const poppins = Poppins({ subsets: ['latin'],weight:["400","500","600","700"] });
 
 const ProductItem = () => {
+
+  const [isHover,setIsHover] = useState(false);
+  const handleHover:MouseEventHandler<HTMLDivElement> = (e) => {
+    console.log(e);
+    setIsHover(true);
+  };
+  const handleMouseLeave:MouseEventHandler<HTMLDivElement> = (e) => {
+    console.log(e);
+    setIsHover(false);
+  };
   return (
-    <div className="w-[285px] h-[446px] bg-[#F4F5F7]">
+    <div className={cn("w-[285px] h-[446px] bg-[#F4F5F7] relative")} onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
+        {
+          isHover && (
+            <div className={cn(poppins.className,"absolute top-0 left-0 w-[285px] h-[446px] bg-[#3A3A3A] bg-opacity-70 flex flex-col items-center justify-center gap-4")}>
+              <button className="h-[48px] w-[202px] bg-white flex items-center justify-center focus:outline-none text-primarySite font-semibold">
+                Add To Cart
+              </button>
+              <div className="flex items-center text-white gap-4">
+                <div className="flex items-center gap-2 font-semibold">
+                  <Share size={16} color="#fff" strokeWidth={3} />
+                  <p>Share</p>
+                </div>
+                <div className="flex items-center gap-2 font-semibold">
+                  <ArrowLeftRight size={16} color="#fff" strokeWidth={3} />
+                  <p>Compare</p>
+                </div>
+                <div className="flex items-center gap-2 font-semibold">
+                  <Heart size={16} color="#fff" strokeWidth={3} />
+                  <p>Like</p>
+                </div>
+              </div>
+            </div>
+          )
+        }
         <Image src="/image 1.png" alt="product" width={285} height={301} objectFit="cover" />
-        <div className="w-full h-full flex flex-col gap-4 pt-4 pl-4">
+        <div className="w-full flex flex-col gap-4 pt-4 pl-4">
             <p className={cn(poppins.className,"text-black font-semibold text-2xl")}>Syltherine</p>
             <p className={cn(poppins.className,"font-medium text-black opacity-50")}>Stylish cafe chair</p>
             <p className={cn(poppins.className,"text-black font-semibold text-lg")}>
--- components/landing/products-grid.tsx
+++ components/landing/products-grid.tsx
@@ -1,8 +1,16 @@
+import { cn } from "@/lib/utils";
 import ProductItem from "./product-item";
+import { Poppins } from "next/font/google";
+const poppins = Poppins({ subsets: ['latin'],weight:["400","500","600","700"] });
 
 const ProductsGrid = () => {
   return (
-    <div className="flex justify-center items-center px-16 py-12 w-full">
+    <div className="flex flex-col gap-8 justify-center items-center px-16 py-12 w-full bg-white">
+      <div className="text-center text-black flex flex-col">
+            <h2 className={cn(poppins.className,"text-5xl font-bold text-[#3A3A3A]")}>
+                Our Products
+            </h2>
+        </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-screen-2xl w-full">
 
         <ProductItem />
@@ -13,8 +21,10 @@ const ProductsGrid = () => {
         <ProductItem />
         <ProductItem />
         <ProductItem />
-        <ProductItem />
       </div>
+      <button className={cn(poppins.className,"w-[245px] h-[48px] bg-white flex items-center justify-center focus:outline-none text-primarySite font-semibold border-[2px] border-primarySite")}>
+        Show More
+      </button>
     </div>
   );
 };

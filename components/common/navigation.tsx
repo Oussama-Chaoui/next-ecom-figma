280e4e2c93f5e98ac101de4fb0262b55af794042
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-03 17:50:51 +0100

    added font for navigation menu

--- components/common/navigation.tsx
+++ components/common/navigation.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 const links = [
   { href: "/", label: "Home" },
@@ -7,13 +9,15 @@ const links = [
   { href: "/", label: "About" },
   { href: "/", label: "Contact" },
 ];
+const poppins = Poppins({ subsets: ['latin'],weight:["400","500","600","700"] });
+
 
 const Navigation = () => {
   return (
     <div className="flex items-center gap-8 md:flex-row flex-col">
       {links.map(({ href, label },index) => (
         <Link href={href} key={index}>
-          <Button className="text-black font-medium text-base" variant={"link"}>
+          <Button className={cn(poppins.className,"text-black font-medium text-base")} variant={"link"}>
             {label}
           </Button>
         </Link>

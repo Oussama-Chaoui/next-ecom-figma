dc775a247de95de2115b786869348b6f009d1493
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-11 16:25:58 +0100

    responsive hero section

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -6,17 +6,17 @@ const Hero = () => {
   return (
     <div className="h-screen w-full">
         <Image src="/hero.png" alt="hero" layout="fill" objectFit="cover" />
-        <div className="h-1/2 w-[40%] absolute top-1/3 right-40 bg-primaryLight text-black rounded-[10px] px-12 pt-20" >
+        <div className="md:h-[500px] md:w-[600px] h-[430px] w-[320px] sm:w-[400px] sm:h-[380px] absolute top-1/3 lg:right-40 right-8 bg-primaryLight text-black rounded-[10px] px-6 pt-6 md:px-12 md:pt-20" >
             <h2 className={cn(poppins.className,"text-base font-semibold tracking-widest mb-4")}>
               New Arrival
             </h2>
-            <h1 className={cn(poppins.className,"text-6xl text-primarySite font-bold mb-2")}>
+            <h1 className={cn(poppins.className,"md:text-6xl text-4xl text-primarySite font-bold mb-2")}>
               Discover Our
             </h1>
-            <h1 className={cn(poppins.className,"text-6xl text-primarySite font-bold mb-4")}>
+            <h1 className={cn(poppins.className,"md:text-6xl text-4xl text-primarySite font-bold mb-4")}>
               New Collection
             </h1>
-            <p className={cn(poppins.className,"font-medium text-lg tracking-wide mb-8")}>
+            <p className={cn(poppins.className,"font-medium md:text-lg text-base tracking-wide mb-8")}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.
             </p>
             <button className={cn(poppins.className,"text-white font-bold tracking-widest uppercase bg-primarySite px-[72px] py-[25px] hover:text-black focus:outline-none")}>

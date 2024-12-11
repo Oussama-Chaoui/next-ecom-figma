b9bb94eec7e402d20d2f9a1db215dd357e9ae142
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-11 16:51:56 +0100

    responsive grid of products

--- components/landing/products-grid.tsx
+++ components/landing/products-grid.tsx
@@ -5,13 +5,13 @@ const poppins = Poppins({ subsets: ['latin'],weight:["400","500","600","700"] })
 
 const ProductsGrid = () => {
   return (
-    <div className="flex flex-col gap-8 justify-center items-center px-16 py-12 w-full bg-white">
+    <div className="flex flex-col gap-8 justify-center items-center md:px-16 px-10 py-12 w-full bg-white">
       <div className="text-center text-black flex flex-col">
-            <h2 className={cn(poppins.className,"text-5xl font-bold text-[#3A3A3A]")}>
+            <h2 className={cn(poppins.className,"md:text-5xl text-4xl font-bold text-[#3A3A3A]")}>
                 Our Products
             </h2>
         </div>
-      <div className="grid gap-y-10 grid-cols-[repeat(1,max-content)] sm:grid-cols-[repeat(2,max-content)] md:grid-cols-[repeat(3,max-content)] lg:grid-cols-[repeat(4,max-content)] justify-between max-w-screen-2xl w-full">
+      <div className="w-full justify-items-center grid gap-y-10 grid-cols-[repeat(1,1fr)] sm:grid-cols-[repeat(2,1fr)] md:grid-cols-[repeat(3,1fr)] lg:grid-cols-[repeat(4,1fr)] justify-between max-w-screen-2xl">
 
         <ProductItem />
         <ProductItem />

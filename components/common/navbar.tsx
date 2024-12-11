e44c00e944c511a7abca9d0971be963e7bd6ec43
Bourichi-Taha <bourichi.taha@gmail.com>
2024-12-11 16:10:33 +0100

    added responsive navbar container and new cart and responsive navbar components

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -1,17 +1,23 @@
-import Icon from "./icon"
-import NavActions from "./nav-actions"
-import Navigation from "./navigation"
+import { Cart } from "./cart";
+import Icon from "./icon";
+import NavActions from "./nav-actions";
+import Navigation from "./navigation";
+import { ResponsiveNavbar } from "./responsive-navbar";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[100px] flex items-center justify-center bg-white fixed top-0 z-50">
-        <div className="w-[90%] h-[41px] flex items-center justify-between">
-            <Icon title="Furniro" />
-            <Navigation />
-            <NavActions />
+    <div className="w-full md:h-[100px] h-16 flex items-center justify-center bg-white fixed top-0 z-50 ">
+      <div className="max-w-screen-2xl h-[41px] flex items-center justify-between sm:px-8 px-2 w-full">
+        <Icon title="Furniro" />
+        <Navigation />
+        <NavActions />
+        <div className="flex gap-1 md:hidden">
+          <Cart />
+          <ResponsiveNavbar />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
